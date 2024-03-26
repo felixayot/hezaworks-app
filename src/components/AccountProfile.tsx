d7391456af8e4e58dbbdc8f9b344f540b5d48b82
thinktank01 <felixayot@gmail.com>
2024-03-26 23:49:45 +0300

    Refactor the user account details viewing component to render dynamic data instead of the earlier implemented hard coded data

--- src/components/AccountProfile.tsx
+++ src/components/AccountProfile.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { JobpostContainer, JobpostTitle, JobpostAttribute } from '../styles/Jobpost.styles';
 import { SignUpFormButton } from '../styles/SignUpFormStyles';
+import axios from '../api/axios'
+
+const ACCPROFILE_URL = 'auth/users/me'
 
 function AccountProfile() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const [usrAcc, setUsrAcc] = useState([]);
+  //const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get(ACCPROFILE_URL, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: false,
+      })
+        .then((response) => {
+            setUsrAcc(response.data);
+            //setIsLoading(false);
+        })
+        .catch((err) => {
+          if (!err?.response) {
+            setError('No response from server');
+          } else {
+            setError(`Failed to fetch user account ${err?.response?.data?.message}`);
+          }
+        });
+  }, []);
+
   function handleEdit() {
     window.location.href = '/account/edit';
   }
 
-  return (
-    <JobpostContainer key='id'>
+  return usrAcc.map(() =>
+    <JobpostContainer>
     <JobpostTitle>My Info</JobpostTitle>
-    <JobpostAttribute><h3>Name</h3>James Barr</JobpostAttribute>
-    <JobpostAttribute><h3>Username</h3>jamesbarr</JobpostAttribute>
-    <JobpostAttribute><h3>Email</h3>jamesbarr@gmail.com</JobpostAttribute>
-    <JobpostAttribute><h3>Account Creation Date</h3>17-03-2024</JobpostAttribute>
+    <JobpostAttribute><h3>Name</h3>{usrAcc.name}</JobpostAttribute>
+    <JobpostAttribute><h3>Username</h3>{usrAcc.username}</JobpostAttribute>
+    <JobpostAttribute><h3>Email</h3>{usrAcc.email}</JobpostAttribute>
+    <JobpostAttribute><h3>User ID</h3>{usrAcc.userID}</JobpostAttribute>
+    <JobpostAttribute><h3>Account Creation Date</h3>{usrAcc.created_at}</JobpostAttribute>
     <SignUpFormButton type="submit" onClick={handleEdit}>Edit My Info</SignUpFormButton>
     </JobpostContainer>
-)}
+  )
+}
 
 export default AccountProfile
