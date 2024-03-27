82e2eb0931369a0d4cdec448c2ef50221998b206
thinktank01 <felixayot@gmail.com>
2024-03-27 22:57:24 +0300

    Refactor the user professional profile component to accept dynamic data entered by logged in users

---
+++ src/styles/UserProfile.styles.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+export const UserProfileContainer = styled.div`
+  display: flex;
+  margin: 20px;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 3vw;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  `;
+
+export const UserProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #fff;
+  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
+`;
+
+export const UserProfileTitle = styled.h2`
+  margin-bottom: 10px;
+  color: #333;
+`;
+
+export const ProfileForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  width: 30vw;
+`;
+
+export const UserProfileInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+export const UserProfileButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: none;
+  border-radius: 5px;
+  background-color: #0795ff;
+  color: #fff;
+  cursor: pointer;
+`;
+
+export const UserProfileText = styled.p`
+  margin-top: 10px;
+  color: #333;
+`;
+
+export const UserProfileLink = styled(Link)`
+  margin-top: 10px;
+  color: #333;
+`;
+
+export const UserProfileTextArea = styled.textarea`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+export const UserProfileSelect = styled.select`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+export const UserProfileOption = styled.option`
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
