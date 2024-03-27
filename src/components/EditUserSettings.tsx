3997de2581acc69722b9f1bc3546dc65cee5e13a
thinktank01 <felixayot@gmail.com>
2024-03-27 22:41:11 +0300

    Revise edit user account settings component name for file naming consistency

---
+++ src/components/EditUserSettings.tsx
@@ -0,0 +1,24 @@
+import { SignUp, SignUpFormButton, SignUpFormContainer, SignUpFormInput, SignUpFormLink, SignUpFormText, SignUpFormTitle, SignUpFormWrapper } from '../styles/SignUpFormStyles'
+
+function EditAccountSettings() {
+  return (
+    <SignUpFormContainer>
+        <SignUpFormWrapper>
+            <SignUp>
+            <SignUpFormTitle>User Account Settings</SignUpFormTitle>
+            <label>Change Password</label>
+            <SignUpFormInput type="password"></SignUpFormInput>
+            <label>Change Email</label>
+            <SignUpFormInput type="email"></SignUpFormInput>
+            <label>Change Username</label>
+            <SignUpFormInput type="text"></SignUpFormInput>
+            <label>Change Profile Picture</label>
+            <SignUpFormInput type="file"></SignUpFormInput>
+            <SignUpFormButton type="submit">Save Changes</SignUpFormButton>
+            </SignUp>
+        </SignUpFormWrapper>
+    </SignUpFormContainer>
+  )
+}
+
+export default EditAccountSettings
