3796810220869e57462156f0bb0e468bb3bedea5
thinktank01 <felixayot@gmail.com>
2024-03-27 22:55:30 +0300

    Bump error pages and loading pages with better CSS styling

---
+++ src/styles/PageLoading.styles.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PageLoadingWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+`;
+
+export const PageLoading = styled.section`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const PageError = styled.section`
+  font-size: 24px;
+  font-weight: bold;
+  color: #F6921E;
+`;
