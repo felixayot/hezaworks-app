3785dcaa2f010f14addc0213a36fd532647b5ee6
thinktank01 <felixayot@gmail.com>
2024-03-31 21:50:32 +0300

    Add an enpoint for users to view talent profiles

---
+++ src/styles/TalentList.styles.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const TalentListContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  margin: 20px;
+  padding: 5px 20px;
+  border-radius: 10px;
+  height: auto;
+  background-color: #f5f5f5;
+`;
+
+export const Title = styled.h1`
+  margin: 20px;
+  color: #27005D;
+`;
+
+export const TalentListTitle = styled.h2`
+  margin-bottom: 10px;
+  color: #333;
+`;
+
+export const TalentListAttribute = styled.p`
+  margin-top: 10px;
+  color: #333;
+`;
+
+export const TalentListLink = styled(Link)`
+    text-decoration: none;
+    /* width: 23.5%;
+    padding: 10px;
+    margin: 10px 0;
+    border: none;
+    border-radius: 5px;
+    background-color: #27005D;
+    color: #fff; */
+    cursor: pointer;
+    
+    &:hover {
+        color: #F6921E;
+        transition: 200ms ease-out;
+    }
+    `;
