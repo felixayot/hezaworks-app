af3e6e998eea733bb3ef5d3db80601a32def0fcb
thinktank01 <felixayot@gmail.com>
2024-03-31 21:54:23 +0300

    Add CSS styles to the applications table

---
+++ src/styles/ApplicationsTable.styles.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const StyledLink = styled(Link)`
+  color: #000;
+  text-decoration: none;
+
+  &:hover {
+    color: #F6921E;
+  }
+`;
+
+export const TableTitle = styled.h2`
+    margin: 20px;    
+`;
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+    margin: 20px 20px;
+`;
+
+export const TableHeader = styled.thead`
+  background-color: #f5f5f5;
+`;
+
+export const TableBody = styled.tbody`
+  background-color: #fff;
+
+`;
+
+export const TableRow = styled.tr`
+  border-bottom: 1px solid #ddd;
+`;
+
+export const TableCell = styled.td`
+  padding: 10px;
+`;
+
+export const TableHeaderCell = styled.th`
+  padding: 10px;
+  text-align: left;
+`;
