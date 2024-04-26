836548b99068e26f1ec5d62cbaca3a4009696e50
thinktank01 <felixayot@gmail.com>
2024-04-26 13:02:46 +0300

    Add component for user login session persistence

---
+++ src/components/PersistLogin.tsx
@@ -0,0 +1,42 @@
+// import { Outlet } from "react-router-dom"
+// import { useState, useEffect } from "react"
+// import useAuth from "../hooks/useAuth"
+// import { PageLoading, PageLoadingWrapper } from "../styles/PageLoading.styles"
+
+// function PersistLogin() {
+//     const [ isLoading, setIsLoading ] = useState(true)
+//     const { auth } = useAuth()
+
+//     useEffect(() => {
+//         const verifyAccessToken = async () => {
+//             try {
+//                 await window.location.reload()
+//                 const accessToken = auth?.accessToken
+//                 auth((prev => {...prev, accessToken }))
+//             }
+
+//             } catch (err) {
+//                 console.log(err)
+//             } finally {
+//                 setIsLoading(false)
+//             }
+//         })
+
+//         !auth.accessToken ? verifyrefreshToken() : setIsLoading(false)
+//     }, [])
+    
+//   return (
+//     <>
+//     {
+//         isLoading ?
+//         <PageLoadingWrapper>
+//             <PageLoading>Loading...</PageLoading>
+//         </PageLoadingWrapper>
+//         :
+//         <Outlet />
+//     }
+//     </>
+//   )
+// }
+
+// export default PersistLogin
