bc08267a2da80320b839fced48ea0c4effe12145
thinktank01 <felixayot@gmail.com>
2024-04-26 13:00:04 +0300

    Add component for the logout feature

---
+++ src/components/Logout.tsx
@@ -0,0 +1,25 @@
+import useAxiosPrivate from "../hooks/UseAxiosPrivate";
+import { HomeButton, HomeLink } from "../styles/HomePage.styles";
+import { PageLoadingWrapper, PageSuccess } from "../styles/PageLoading.styles";
+import PublicNavbar from "./PublicNavbar";
+
+const LOGOUT_URL = "/auth/logout";
+
+function Logout() {
+  const privateAxios = useAxiosPrivate();
+  // const location = useLocation();
+
+  privateAxios.post(LOGOUT_URL, { withCredentials: true });
+
+  return (
+    <>
+    {/* <PublicNavbar /> */}
+  <PageLoadingWrapper>
+    <PageSuccess>You've been logged out successfully.</PageSuccess><br />
+    <HomeButton><HomeLink to="/">Home</HomeLink></HomeButton>
+  </PageLoadingWrapper>
+  </>
+  );
+}
+
+export default Logout;
